31150b282a6aad215ebf7ef7dd0bf6c3e3e24e14
Morzy <934470937@qq.com>
2024-01-29 12:21:25 +0800

    Delete backend directory

--- backend/dist/app.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { ResultDTO } from './result.dto';
-export declare class AppService {
-    getHello(): string;
-    getData(): Promise<ResultDTO[]>;
-}
--- backend/dist/result.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ResultDTO = void 0;
-class ResultDTO {
-}
-exports.ResultDTO = ResultDTO;
-//# sourceMappingURL=result.dto.js.map
